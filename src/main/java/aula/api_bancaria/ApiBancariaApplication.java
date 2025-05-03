1438183d2c4364f76ed16a3a0cffce0f57b0918f
Marco Antônio <marcosynky@gmail.com>
2025-05-03 06:02:03 -0300

    first commit

---
+++ src/main/java/aula/api_bancaria/ApiBancariaApplication.java
@@ -0,0 +1,14 @@
+package aula.api_bancaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBancariaApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ApiBancariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/aula/api_bancaria/ApiBancariaApplicationTests.java
@@ -0,0 +1,13 @@
+package aula.api_bancaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBancariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
